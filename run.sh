e09a9841d5a588f19c88b3fdd46966a02c65f478
Ryan Northey <ryan@synca.io>
2022-09-19 19:06:39 +0100

    Dockerfile

--- run.sh
+++ run.sh
@@ -4,16 +4,17 @@ REPO="$1"
 shift
 
 # This assumes an ssh repo
-mkdir -p /root/.ssh;
+# mkdir -p /root/.ssh;
 
-if [[ -e /root/ssh ]]; then
-    ln -sf /root/ssh/id_rsa /root/.ssh/id_rsa
-fi
+# if [[ -e /root/ssh ]]; then
+#     ln -sf /root/ssh/id_rsa /root/.ssh/id_rsa
+# fi
 
-REPO_URI="$(echo "$REPO" | cut -d@ -f2 | cut -d: -f1)"
-SUFFIX="$(echo "$REPO" | cut -d: -f2)"
+# REPO_URI="$(echo "$REPO" | cut -d@ -f2 | cut -d: -f1)"
+# SUFFIX="$(echo "$REPO" | cut -d: -f2)"
 
-ssh-keyscan -t rsa "$REPO_URI" >> /root/.ssh/known_hosts
+# ssh-keyscan -t rsa "$REPO_URI" >> /root/.ssh/known_hosts
+
 git clone "$REPO" cloned
 cd "cloned/$SUBDIR"
 
